2ab0f0c0ca9bdba129fe20865a3bbf39f18e3382
walidbarakat <walidsalahbarakat@outlook.com>
2021-08-21 17:00:47 +0200

    fix return types for functions

--- unrawpkt.h
+++ unrawpkt.h
@@ -1,8 +1,12 @@
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/in.h>
+#include <linux/ipv6.h>
+#include <linux/icmp.h>
+#include <linux/icmpv6.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+
+#include <linux/in.h>
 
 unsigned char *pkt;
 
@@ -11,22 +15,22 @@ void packet_recv(unsigned char buffer[65536]);
 /*
  * layer 2
  */
-void extract_eth_hdr(unsigned char *raw_pkt);
+struct ethhdr extract_eth_hdr(unsigned char *raw_pkt);
 
 /*
  * layer 3
  */
-void extract_ipv4_hdr(unsigned char *raw_pkt);
+struct iphdr extract_ipv4_hdr(unsigned char *raw_pkt);
 
-void extract_ipv6_hdr(unsigned char *raw_pkt);
+struct ipv6hdr extract_ipv6_hdr(unsigned char *raw_pkt);
 
-void extract_icmpv4_hdr(unsigned char *raw_pkt);
+struct icmphdr extract_icmpv4_hdr(unsigned char *raw_pkt);
 
-void extract_icmpv6_hdr(unsigned char *raw_pkt);
+struct icmpv6hdr extract_icmpv6_hdr(unsigned char *raw_pkt);
 
 /*
  * layer 4
  */
-void extract_tcp_hdr(unsigned char *raw_pkt);
+struct tcphdr extract_tcp_hdr(unsigned char *raw_pkt);
 
-void extract_udp_hdr(unsigned char *raw_pkt);
+struct udphdr extract_udp_hdr(unsigned char *raw_pkt);
